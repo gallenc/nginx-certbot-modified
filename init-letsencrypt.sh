45b145168196f018353d463875b1d8424c048025
gallenc <cgallen@opennms.org>
2023-11-03 10:51:26 +0000

    changed domain name

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -6,7 +6,7 @@
 #  exit 1
 #fi
 
-domains=(openoss1.uksouth.cloudapp.azure.com www.openoss1.uksouth.cloudapp.azure.com)
+domains=(openoss1.uksouth.cloudapp.azure.com)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="cg02r@zepler.net" # Adding a valid address is strongly recommended
